a9dcb1537515cf340d0fd83b2552d5db85585508
Omikunle Ademola <demostat1995@gmail.com>
2024-06-03 08:19:45 -0700

    modified and add

--- src/components/Projects/ProjectArray.jsx
+++ src/components/Projects/ProjectArray.jsx
@@ -5,13 +5,22 @@ import Image2 from "./Images/Capture.png"
 import Image3 from "./Images/superhuman.png"
 import Image4 from "./Images/Calculator.png"
 import Image5 from "./Images/testimonialPic.png"
-
-import Image7 from "./Images/DisneyImage.png"
 import Image6 from "./Images/sailPic.png"
+import Image7 from "./Images/DisneyImage.png"
+import Image8 from "./Images/techcareimage.png"
+
 
 
 export const ProjectArray = [
+    {   ID:8,
+        id:"Health care patient Dashboard Page created with React, where patients details can be found. Created for Pc view only. Coalition Fake API was consumed for this. Pictogram was created with Chartjs. Patients can be searched by name, click on the more(...) on a particular patient to view patient information",
+        visitLogo:<FaInternetExplorer size={15}/>,
+        visitLink:"https://health-care-kucbc5wb2-ademola-omikunles-projects.vercel.app/",
+        repoLogo:<FaGithub size={15}/>,
+        repoLink:"https://github.com/Demostat101/healthCare",
+        image: Image8
 
+    },
     {   ID:7,
         id:"Responsive Search Engine created with React. Google API was consumed for this project",
         visitLogo:<FaInternetExplorer size={15}/>,
