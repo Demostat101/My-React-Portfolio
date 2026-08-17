606422302c020be081ce5e436b10d515ac8f825a
Demostat101 <demostat1995@gmail.com>
2026-08-17 17:53:31 -0600

    added iframe

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,40 +1,99 @@
-import React from "react";
-import MyImage from "../../assets/images/my_pic1.jpg"
-import { useTheme } from '../../contextComponent/ThemeContext'
-import "./About.css"
+// import React from "react";
+// import MyImage from "../../assets/images/my_pic1.jpg"
+// import { useTheme } from '../../contextComponent/ThemeContext'
+// import "./About.css"
+
+// const About = () => {
+//     const{theme} = useTheme()
+//   return (
+//     <div id="about2">
+//       <div className="aboutMe">
+//         <div className="abouthead">
+//           <span className="head1">ABOUT</span>{" "}
+//           <span className="head2">
+//             ME
+//           </span>
+//         </div>
+//       </div>
+
+//       <div className="aboutbody">
+
+//         <div className={theme==="light"?"imageContainer":"imageContainerDark"}><img src={MyImage} alt="" className="img"/></div>
+
+//         <div className="paragraph">
+//           <h2 className="myName">
+//             <span className="my">
+//               My Name Is
+//             </span>
+//             <span className="ademola"> Omikunle Ademola</span>
+//           </h2>
+
+//           <p className="parabody">
+//             I am passionate about creating responsive, user-friendly websites
+//             that are both visually appealing and functional. No matter what the
+//             scope of the project is, I am committed to building high-quality
+//             websites that meet the needs of my clients and their customers.
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
+
+
+
+import MyImage from "../../assets/images/my_pic1.jpg";
+import { useTheme } from "../../contextComponent/ThemeContext";
+import "./About.css";
 
 const About = () => {
-    const{theme} = useTheme()
+  const { theme } = useTheme();
+
   return (
     <div id="about2">
       <div className="aboutMe">
         <div className="abouthead">
           <span className="head1">ABOUT</span>{" "}
-          <span className="head2">
-            ME
-          </span>
+          <span className="head2">ME</span>
         </div>
       </div>
 
       <div className="aboutbody">
-
-        <div className={theme==="light"?"imageContainer":"imageContainerDark"}><img src={MyImage} alt="" className="img"/></div>
+        <div
+          className={
+            theme === "light"
+              ? "imageContainer"
+              : "imageContainerDark"
+          }
+        >
+          <img src={MyImage} alt="" className="img" />
+        </div>
 
         <div className="paragraph">
           <h2 className="myName">
-            <span className="my">
-              My Name Is
-            </span>
+            <span className="my">My Name Is</span>
             <span className="ademola"> Omikunle Ademola</span>
           </h2>
 
           <p className="parabody">
-            I am passionate about creating responsive, user-friendly websites
-            that are both visually appealing and functional. No matter what the
-            scope of the project is, I am committed to building high-quality
-            websites that meet the needs of my clients and their customers.
+            I am passionate about creating responsive, user-friendly
+            websites that are both visually appealing and functional.
+            No matter what the scope of the project is, I am committed
+            to building high-quality websites that meet the needs of my
+            clients and their customers.
           </p>
         </div>
+      </div>
+
+      {/* Candidates Application */}
+      <div className="candidates-embed">
+        <iframe
+          src="http://localhost:5173/?slug=maryam-technologies-limited&theme=%23395A7F"
+          title="Candidates Embed"
+          className="candidates-iframe"
+        />
       </div>
     </div>
   );
